2af5fbe35aee71a8edf501d7cd6ae7ba84a9d1b4
tbauer42 <trist57890@gmail.com>
2020-06-30 17:40:46 +0200

    add menu and twigpath for URL

--- App_1/src/Controller/PersonnageController.php
+++ App_1/src/Controller/PersonnageController.php
@@ -8,18 +8,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class PersonnageController extends AbstractController
 {
     /**
-     * @Route("/", name="personnage")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
         return $this->render('personnage/index.html.twig');
     }
-    
     /**
-     * @Route("/persos", name="persos")
+     * @Route("/persos", name="personnages")
      */
     public function persos()
     {
-        return $this->render('personnage/persos.html.twig');
+        return $this->render('personnage/persos.html.twig', [
+            "pseudo" => "toto",
+            "age"    => 25,
+            "carac" => [
+                "force" => 3,
+                "agi"   => 2,
+                "intelligence" => 3
+
+            ]
+        ]);
     }
 }
