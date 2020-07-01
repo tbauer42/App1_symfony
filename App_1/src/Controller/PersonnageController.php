3355dc66b6d01346e24d6cbf7bce62ff83f9c29b
tbauer42 <trist57890@gmail.com>
2020-07-01 19:06:59 +0200

    add perso list

--- App_1/src/Controller/PersonnageController.php
+++ App_1/src/Controller/PersonnageController.php
@@ -19,15 +19,45 @@ class PersonnageController extends AbstractController
      */
     public function persos()
     {
-        return $this->render('personnage/persos.html.twig', [
-            "pseudo" => "toto",
+        $j1 = [
+            "pseudo" => "Marc",
             "age"    => 25,
+            "sexe"   => true,
             "carac" => [
                 "force" => 3,
                 "agi"   => 2,
                 "intelligence" => 3
+        ]
+            ];
 
-            ]
+        $j2 = [
+            "pseudo" => "Milo",
+            "age"    => 30,
+            "sexe"   => true,
+            "carac" => [
+                "force" => 5,
+                "agi"   => 1,
+                "intelligence" => 2
+        ]
+            ];
+
+        $j3 = [
+            "pseudo" => "Tya",
+            "age"    => 22,
+            "sexe"   => false,
+            "carac" => [
+                "force" => 1,
+                "agi"   => 2,
+                "intelligence" => 5
+        ]
+            ];
+        $players = [
+            "j1" => $j1,
+            "j2" => $j2,
+            "j3" => $j3
+        ];
+        return $this->render('personnage/persos.html.twig', [
+            "players" => $players
         ]);
     }
 }
